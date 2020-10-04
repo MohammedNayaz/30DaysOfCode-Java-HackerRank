a36e5b8a57ba91e58ab3f464ab96a43f02c8de23
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-04 11:00:42 +0530

    Add files via upload

---
+++ Day 16/Day 16 Exceptions String to Integer.java
@@ -0,0 +1,24 @@
+//Day 16 Exceptions - String to Integer
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try 
+        {
+            System.out.println(Integer.parseInt(S));
+        }
+        catch (NumberFormatException ex) 
+        {
+            System.out.println("Bad String");
+        }
+    }
+}
